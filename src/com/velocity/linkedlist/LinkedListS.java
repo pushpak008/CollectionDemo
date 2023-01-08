b92bbd8ba28276d90dc233a9802b3c73e2a78855
pushpak008 <pushpakcoolkarni@gmail.com>
2023-01-08 13:49:13 +0530

    Added LinkedList

---
+++ src/com/velocity/linkedlist/LinkedListS.java
@@ -0,0 +1,16 @@
+package com.velocity.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListS {
+	public static void main(String[] args) {
+		List<String> list=new LinkedList();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		System.out.println(list);
+
+	}
+
+}
