23e88929e4b4703e2755b1d57d27f58ec9e70b98
pushpak008 <pushpakcoolkarni@gmail.com>
2023-01-08 13:42:39 +0530

    first commit

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("ajay");
+		list.add("vijay");
+		list.add("ram");
+		list.add("samir");
+		System.out.println(list);
+	}
+
+}
